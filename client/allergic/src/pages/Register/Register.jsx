07acdfd1f4ecdafd005cb255ad8cb27b8e6a859d
Alba <alba.m.navarro@gmail.com>
2022-02-08 14:46:39 +0100

    update de medio dia

--- client/allergic/src/pages/Register/Register.jsx
+++ client/allergic/src/pages/Register/Register.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import FormUser from '../../components/FormUser/FormUser';
+import FormEmergency from '../../components/FormEmergency/FormEmergency';
 
 const Register = () => {
   return <div> Dinos quién eres:
   <FormUser/>
   </div>;
+  <div> <h3>Vamos a añadir tu contacto en caso de emergencia</h3>
+  <p>Nos pondremos en contacto con tu persona de confianza y/o compañia de seguros en caso de emergencia.</p> 
+  <FormEmergency/>
+  </div>
+  
 };
 
 export default Register;
