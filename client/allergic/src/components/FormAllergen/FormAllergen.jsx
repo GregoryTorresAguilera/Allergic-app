557c5a4070b8044deacdc120eadc36ff9bf25cf0
Alba <alba.m.navarro@gmail.com>
2022-02-08 15:42:17 +0100

    update

---
+++ client/allergic/src/components/FormAllergen/FormAllergen.jsx
@@ -0,0 +1,4 @@
+/// vamos a hacer un fetch recogiendo alergenos (id y name) los pintamos en botones -----> 
+
+/// cuando clicas los botones te pinta 
+///local storage que se suma al array de objetos que mandamos al submit para post user
