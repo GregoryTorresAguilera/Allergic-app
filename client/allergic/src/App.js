5fd69dd56be8f5c21eec6dd791cfa18a864399d9
Samuel-G3 <sgarciaine@gmail.com>
2022-02-08 12:31:48 +0100

    login page component

--- client/allergic/src/App.js
+++ client/allergic/src/App.js
@@ -7,6 +7,7 @@ import RestMap from './pages/RestMap/RestMap';
 import Emergency from './pages/Emergency/Emergency';
 import Favorite from './pages/Favorite/Favorite';
 import Diary from './pages/Diary/Diary';
+import Login from './pages/Login/Login';
 
 
 function App() {
@@ -20,6 +21,8 @@ function App() {
           <Route path="/emergency" element={<Emergency />} />
           <Route path="/favorite" element={<Favorite />} />
           <Route path="/diary" element={<Diary />} />
+          <Route path="/login" element={<Login />} />
+
 
           {/* <Route path="characters" element={<CharactersPage />} />
           <Route path="characters/:id" element={<CharacterDetailPage />} />
